0436ee0489e82ad1dc21920f6d24091a7dd6d64d
Kane Pangalia Stephens <kanepstephens@gmail.com>
2019-09-03 18:04:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,6 +18,8 @@ def get_japanese_emoticon(file, emoticon)
   new_hash[:get_emoticon].each_pair { |english_emoticon, japanese_emoticon| 
     if english_emoticon == emoticon then
       return japanese_emoticon
+    else
+      
     end 
   }
   
