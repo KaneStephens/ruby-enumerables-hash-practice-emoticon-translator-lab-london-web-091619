42597b1724cab76d49796d139f56fccb32e9b4d7
Kane Pangalia Stephens <kanepstephens@gmail.com>
2019-09-03 17:42:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,11 @@
 require "yaml"
 
 def load_library(file)
-
   emoticons_hash = YAML.load_file(file)
+  final_hash = {:get_meaning, :get_emoticon}
   
   print(emoticons_hash)
-  return emoticons_hash
+  return final_hash
 end
 
 def get_japanese_emoticon
